be88099d249ba46a685b4c7b2dd23c8424d2d50a
Bogachev <bogachok-m@mail.ru>
2021-03-11 23:33:04 +0300

    add folders for MVVM

---
+++ StalingradTimer/View/TimerScreen.swift
@@ -0,0 +1,20 @@
+//
+//  TimerScreen.swift
+//  StalingradTimer
+//
+//  Created by mac on 11.03.2021.
+//
+
+import SwiftUI
+
+struct TimerScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TimerScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        TimerScreen()
+    }
+}
