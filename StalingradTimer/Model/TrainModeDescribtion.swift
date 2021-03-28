032d57e2783879adf53bf3613f775fd3b0c3d53e
Bogachev <bogachok-m@mail.ru>
2021-03-28 14:01:36 +0300

    Add new model - TrainModeDescribtion

---
+++ StalingradTimer/Model/TrainModeDescribtion.swift
@@ -0,0 +1,14 @@
+//
+//  TrainModeDescribtion.swift
+//  StalingradTimer
+//
+//  Created by mac on 28.03.2021.
+//
+
+enum TrainModeDescribtion: String {
+    case initial = ""
+    case prepare = "Подготовка"
+    case work = "Тренировка"
+    case rest = "Отдых"
+    case paused = "Пауза"
+}
